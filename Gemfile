93662200204af8775fa4b2306d10c943cb785ea6
viktor weinand <weinand.viktor@gmail.com>
2018-11-14 17:01:45 +0100

    pused error

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'faker'
 
 gem 'carrierwave', '~> 1.2'
 gem 'cloudinary'
+gem "highcharts-rails"
 
 
 group :development do
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,10 @@ import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 // import { mapElement } from '../components/map';
 // mapElement();
+
+//= require highcharts
+
+import { myChart } from '../components/highcharts';
+
+myChart();
+

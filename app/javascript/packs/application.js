d24b1deaa0fb7c9cda73b3ee7db15d702e4ad2bd
viktor weinand <weinand.viktor@gmail.com>
2018-11-16 17:22:49 +0100

    zazea

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,16 @@
+import "bootstrap";
 import Chartkick from "chartkick"
 window.Chartkick = Chartkick;
-
+import $ from 'jquery';
+import "bootstrap-datepicker"
 // for Chart.js
 import Chart from "chart.js";
 Chartkick.addAdapter(Chart);
+
+   $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+
 
 // import { myChart } from '../components/highcharts';
 // myChart();
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -7,6 +7,8 @@ const mapElement = document.getElementById('map');
 
 if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = process.env.MAPBOX_API_KEY; // API key from `.env`
+
+
   const map = new mapboxgl.Map({
     container: 'map',
      style: 'mapbox://styles/mapbox/streets-v10'

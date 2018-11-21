9fb1370026568f268342a4534929302246bc65c2
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2018-11-21 15:59:02 +0100

    debugged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:student, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_student, :name])
   end
 
 end

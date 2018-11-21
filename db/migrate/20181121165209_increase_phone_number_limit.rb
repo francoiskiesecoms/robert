dd571e8a49051a46623879e909b1092565342fc4
viktor weinand <weinand.viktor@gmail.com>
2018-11-21 17:55:57 +0100

    phone done

--- db/migrate/20181121165209_increase_phone_number_limit.rb
+++ db/migrate/20181121165209_increase_phone_number_limit.rb
@@ -1,5 +1,5 @@
 class IncreasePhoneNumberLimit < ActiveRecord::Migration[5.2]
   def change
-    change_column :student, :phone_number, :integer, limit: 8
+    change_column :students, :phone_number, :integer, limit: 8
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_151418) do
+ActiveRecord::Schema.define(version: 2018_11_21_165209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_151418) do
     t.string "address"
     t.string "studies"
     t.string "school"
-    t.integer "phone_number"
+    t.bigint "phone_number"
     t.string "photo"
     t.string "email"
     t.string "motivation"
